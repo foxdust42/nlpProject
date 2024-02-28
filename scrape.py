926f13ab9e7fca70fcb5e1013b3be467744f388b
foxdust42 <foxdust42@gmail.com>
2024-02-28 23:28:37 +0100

    replaced HTMLParser with Beautiful Soup; finalized scraper

--- scrape.py
+++ scrape.py
@@ -1,43 +1,8 @@
 import json
 import requests as r
-from html.parser import HTMLParser
-
-class BangladeshAccidentScraper(HTMLParser):
-    # the .upper-box class has a single instance and it contains tha title + some metadata 
-    in_upper_box : bool = False
-    in_title : bool = False
-    
-    def __init__(self, *, convert_charrefs: bool = True) -> None:
-        super().__init__(convert_charrefs=convert_charrefs)
-    
-    def restore(self):
-        self.in_upper_box = False
-        self.in_title = False
-    
-    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
-        #return super().handle_starttag(tag, attrs)
-        # print ("Tag:", tag)
-        # for attr in attrs:
-        #     print ("    attr: ", attr)
-        #     print(attr[0])
-        # print("\n") 
-        # if attrs is not None and len(attrs) >= 1:
-        #     print("A::", attrs[0][0], attrs[0][1])
-        if attrs is not None and len(attrs) >= 1 and attrs[0][0] == "class" and attrs[0][1] == "upper-box":
-            self.in_upper_box = True
-            return
-        if self.in_upper_box == True and tag == "h2":
-            self.in_title = True;
-            return
-    
-    def handle_data(self, data: str) -> None:
-        #return super().handle_data(data)
-        if self.in_title == True:
-            print("Title:", data.strip())
-            self.in_title = False
-            self.in_upper_box = False
-            return
-    
+import bs4
+import csv 
+#from bs4 import BeautifulSoup
 
 with open("urls.json", "r") as f:
     d = json.load(f)
@@ -46,14 +11,50 @@ with open("urls.json", "r") as f:
 if d is None:
     raise IOError
 
-print(d[0])
+csvfile = open('articles.csv', 'w', newline='')
 
-req = r.get(d[0])
+if csvfile is None:
+    raise IOError
 
-#print(req.text)
-print('Resp =', req.status_code)
+out = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_ALL)
+    
+print("\a\n")
 
-parser = BangladeshAccidentScraper()
+for i in range(0, len(d)):
 
-parser.feed(req.text)
+    #print(d[i])
 
+    req = r.get(d[i])
+
+    #print(req.text)
+    #print('Resp =', req.status_code)
+
+    # parser = BangladeshAccidentScraper()
+
+    # parser.feed(req.text)
+
+    soup = bs4.BeautifulSoup(req.text, 'html.parser')
+
+    title = soup.h2.text
+    meta_location = soup.find("span", class_ = "icon fa fa-map-marker").parent.contents[1]
+    meta_publish = soup.find("span", class_ = "icon qb-clock").parent.contents[1]
+    #print(meta_publish)
+
+    text_html = soup.find(class_ = "text").findChild("div")
+
+    garbage = text_html.__str__()
+
+    children = text_html.findChildren("p", recursive=False)
+
+    for child in children:
+        if child.find("a") is not None:
+            a ="".join([t for t in child.contents if type(t)==bs4.element.NavigableString])
+            if a in ["Read more: ", "Read: ", ""]:
+                child.decompose()
+            
+    #print(text_html.get_text())
+    out.writerow([d[i], meta_publish, meta_location, title, garbage, text_html.get_text()])
+
+csvfile.close()
+
+print("All is done\a\n")
